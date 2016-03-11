5fa43ed38c7666dc7252bab4b920f54042786d5f
Gabriel <gavazquez@gmail.com>
2016-03-11 17:51:48 +0100

    Create Program.cs

---
+++ CSharp/Program.cs
@@ -0,0 +1,16 @@
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = "server.myserver.us";
+            var port = 9999;
+            var username = "user";
+            var password = "pass";
+            var desKey = new byte[14] { 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14 };
+
+            new NlineTester().TestNline(server, port, username, password, desKey);
+        }
+    }
+}
