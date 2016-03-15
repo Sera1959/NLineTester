733e0e4d92d8e17f46db16532d3f8c2709d6d92c
Gabriel <gavazquez@gmail.com>
2016-03-15 21:03:04 +0100

    Create Main.py

---
+++ Python/Main.py
@@ -0,0 +1,10 @@
+ #!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+   
+if __name__ == "__main__":
+    import NlineTester
+    
+    nline = "N: my.server.com 999 username password"
+    configKey = "01 02 03 04 05 06 07 08 09 10 11 12 13 14"
+
+    NlineTester.TestNline(nline, configKey)
