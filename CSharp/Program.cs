6513f0a48ffcddf1b1e9db3d703b6bf76ddc2246
Gabriel <gavazquez@gmail.com>
2016-03-15 14:36:43 +0100

    Update Program.cs

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -8,9 +8,9 @@ namespace ConsoleApplication
             var port = 9999;
             var username = "user";
             var password = "pass";
-            var desKey = new byte[14] { 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14 };
-
-            new NlineTester().TestNline(server, port, username, password, desKey);
+            var configKey = "01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14";
+            
+            new NlineTester().TestNline(server, port, username, password, configKey);
         }
     }
 }
