5cfdce196f88b2da984c7d39337023ff3a370679
Gabriel <gavazquez@gmail.com>
2016-03-16 12:24:35 +0100

    Update Main.py

--- Python/Main.py
+++ Python/Main.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python
+ #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 #Created by Dagger -- https://github.com/DaggerES
-   
+
 if __name__ == "__main__":
     import NlineTester
     
-    nline = "N: my.server.com 999 username password"
-    configKey = "01 02 03 04 05 06 07 08 09 10 11 12 13 14"
+    nline = "N: my.server.com 9999 username password 01 02 03 04 05 06 07 08 09 10 11 12 13 14"
 
-    NlineTester.TestNline(nline, configKey)
+    NlineTester.TestNline(nline)
