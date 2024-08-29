2072562f5e0909a368d95d3b935f3db27aac4392
Sera1959 <sesancalvo1@gmail.com>
2024-08-29 09:05:34 +0200

    Update Main.py

--- Python/Main.py
+++ Python/Main.py
@@ -3,8 +3,8 @@
 #Created by Dagger -- https://github.com/gavazquez
 
 if __name__ == "__main__":
-    import NlineTester
-    
+    top1.starcline.net 18801 DFGF0816 89816877
+     
     nline = "N: my.server.com 9999 username password 01 02 03 04 05 06 07 08 09 10 11 12 13 14"
 
     NlineTester.TestNline(nline)
